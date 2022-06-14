f0c8763e97c3555a69cf5168fc81733bcbf43f73
manoj <parkalemanojc@gmail.com>
2022-06-14 16:29:17 +0530

    new one

--- demoGithub/src/main/java/com/example/demo/DemoGithubApplication.java
+++ demoGithub/src/main/java/com/example/demo/DemoGithubApplication.java
@@ -8,7 +8,7 @@ public class DemoGithubApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoGithubApplication.class, args);
-		System.out.println("hello github");
+		System.out.println("hello github developer");
 	}
 		
 }
